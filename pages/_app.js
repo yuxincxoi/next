ff5fda966d9d48ac13e6d2d0b8213c5252bc6a5d
Yujin <yjyj0774@gmail.com>
2022-08-12 23:18:32 +0900

    title & navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 
 export default function App({Component, pageProps}) {
     return (
-        <>
-            <NavBar />
+        <Layout>
             <Component {...pageProps} />
-            <style jsx global>{`
-                a {
-                    color: white;
-                }
-            `}</style>
-        </>
+        </Layout>
     )
 }
